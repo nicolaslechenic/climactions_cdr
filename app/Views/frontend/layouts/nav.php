19de45b32f97b5731a31479cfa7bd190c36a373d
cdbschr <c.debusscher@gmail.com>
2022-05-09 10:40:25 +0200

    🐛 FIX: link logo

--- app/Views/frontend/layouts/nav.php
+++ app/Views/frontend/layouts/nav.php
@@ -2,19 +2,19 @@
 require_once './app/Views/frontend/layouts/header.php'; ?>
 
 <header id="bandeau">
-    <nav id="navigation-bandeau" class="container">
+  <nav id="navigation-bandeau" class="container">
 
-        <img src="./Public/img/logo_clim_action.png" alt="Logo">
+    <a href="/"><img src="./Public/img/logo_clim_action.png" alt="Logo"></a>
 
 
-        <ul id="menu" class="hidden">
-            <li><a href="#">Accueil</a></li>
-            <li><a href="#">Ressources</a></li>
-            <li><a href="#">évènements</a></li>
-            <li><a href="#">Retourner sur le site</a></li>
-        </ul>
+    <ul id="menu" class="hidden">
+      <li><a href="#">Accueil</a></li>
+      <li><a href="#">Ressources</a></li>
+      <li><a href="#">évènements</a></li>
+      <li><a href="#">Retourner sur le site</a></li>
+    </ul>
 
-        <img id="burger" src="./Public/img/burger.png" alt="burger">
+    <img id="burger" src="./Public/img/burger.png" alt="burger">
 
-    </nav>
+  </nav>
 </header>
