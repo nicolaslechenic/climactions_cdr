a02243c0981908d74375b9dbc16f95cffd8b097e
bigben35 <79405849+bigben35@users.noreply.github.com>
2022-05-24 10:30:03 +0200

    chnage branch, non fini

--- app/Views/admin/home.php
+++ app/Views/admin/home.php
@@ -2,5 +2,12 @@
 
 <h1>Bienvenue</h1>
 
+<h2>Liste des administrateurs :</h2>
+
+<?php foreach($listAdmin as $admin) : ?>
+
+
+<?php endforeach ?>
+
 <?php $content = ob_get_clean(); ?>
 <?php require 'layouts/dashboard.php'; ?>
