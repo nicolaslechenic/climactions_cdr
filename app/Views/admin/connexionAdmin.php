61d89106a62617e9e645b0602dcedde0d943e43a
bigben35 <79405849+bigben35@users.noreply.github.com>
2022-05-30 11:48:34 +0200

    📦 NEW: clim-85  front de la page de connexion responsive

--- app/Views/admin/connexionAdmin.php
+++ app/Views/admin/connexionAdmin.php
@@ -4,13 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Public/admin/css/style.css">
     <title>Connexion</title>
 </head>
 <body>
+    <section id="section-connectAdmin">
+    <h1>Connexion Administrateur</h1>
    <form id="form_admin" action="indexAdmin.php?action=home" method="post">
 
         <div>
-            <label for="email"> email:</label>
+            <label for="email"> Email:</label>
             <input type="email" name="email" id="email" placeholder="Votre email" required>
         </div>
 
@@ -18,9 +21,10 @@
             <label for="password"> Mot de Passe:</label>
             <input type="password" name="password" id="password" placeholder="Votre mot de passe" required>
         </div>
-        <button type="submit">Connexion</button>
+        <button class="btnConnexion" type="submit">Connexion</button>
         <span class="psw"><a href="indexAdmin.php?action=forgot_password">Mot de passe oublié ?</a></span>
     </form>
+    </section>
 </body>
 </html>
 
